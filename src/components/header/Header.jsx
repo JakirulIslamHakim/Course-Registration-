ec79954c93183555231d8b32c30859abba4bd8a0
Hakim <jakirulislamhakim@gmail.com>
2023-09-14 22:18:41 +0600

    fake data & header complete

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='text-3xl font-bold text-center'>
+            <h3>Course Registration</h3>
+        </div>
+    );
+};
+
+export default Header;
